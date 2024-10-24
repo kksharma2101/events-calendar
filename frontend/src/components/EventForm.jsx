c378eea007b186434328858ceacd90ae98963633
kamal sharma <kamalbsr2101@gmail.com>
2024-10-24 23:17:13 +0530

    add changes

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -1,46 +1,50 @@
 // components/EventForm.js
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import ".././App.css"
 
 const EventForm = () => {
   const [eventData, setEventData] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-        try {
-            const response = await axios.get("http://localhost:5000/api/get-events");
-            console.log(response.data.events);
-            setEventData(response.data.events);
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    getData()
-  }, ['']);
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/get-events"
+        );
+        console.log(response.data.events);
+        setEventData(response.data.events);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, [""]);
 
   return (
     <>
-      {/* <div className=""> */}
+      <div className="row row-cols-1 row-cols-md-3 g-4 px-4">
         {eventData?.map((item) => (
           <ul key={item.id}>
-            <li>{item.title} </li>
-            <li>{item.discription} </li>
-            <li>{item.date} </li>
-            {/* <div class="card border-success mb-3" style="max-width: 18rem;">
-              <div class="card-header bg-transparent border-success">
-                {item.title}
+            <div
+              className="card border-success mb-3 mt-4"
+              style={{ maxWidth: "18rem" }}
+            >
+              <div className="card-header bg-transparent border-success">
+                <h3 className="card-title">{item.title}</h3>
+                <p>{item.date.slice(0, 10)}</p>
               </div>
-              <div class="card-body text-success">
-                <h5 class="card-title">Success card title</h5>
-                <p class="card-text">{item.discription}</p>
+              <div className="card-body text-success">
+                <p className="card-text">{item.discription}</p>
               </div>
-              <div class="card-footer bg-transparent border-success">
-                {item.date}
+              <div className="card-footer bg-transparent border-success d-flex justify-content-around">
+                <button className="eventForm-upt-btn">Update</button>
+                <button className="eventForm-dlt-btn">Delete</button>
               </div>
-            </div> */}
+            </div>
           </ul>
         ))}
-      {/* </div> */}
+      </div>
     </>
   );
 };
